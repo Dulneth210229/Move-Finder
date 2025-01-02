41d333cfdd79f4179dbabd5b8934f6726d564690
Dulneth210229 <dulneth.sa@gmail.com>
2025-01-02 20:56:28 +0530

    fetch the data using http request using the OMDb API

--- script.js
+++ script.js
@@ -4,4 +4,24 @@
 document.addEventListener("DOMContentLoaded", function () {
   const movieForm = document.getElementById("movieForm");
   const movieResult = document.getElementById("movieResults");
+
+  movieForm.addEventListener("submit", (e) => {
+    const movieName = document.getElementById("movieInput").value;
+    //prevent the browser default
+    e.preventDefault();
+    searchMovies(movieName);
+  });
+
+  //search for movies
+  async function searchMovies(movieName) {
+    console.log(movieName);
+
+    try {
+      //loading
+      movieResult.innerHTML = '<div class="loading">Searching movies...</div>';
+      const response = await fetch(
+        `http://www.omdbapi.com/?apikey=2ecc7029&s=${movieName}`
+      );
+    } catch (error) {}
+  }
 });
