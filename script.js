a390dc1b02abf2b095c44b078969210b83cc5e4c
Dulneth210229 <dulneth.sa@gmail.com>
2025-01-02 20:31:21 +0530

    initial commit of js file

---
+++ script.js
@@ -0,0 +1,6 @@
+//http://www.omdbapi.com/?apikey=2ecc7029&s
+//http://www.omdbapi.com/?apikey=2ecc7029&s=batman
+
+document.addEventListener("DOMContentLoaded", function () {
+  const movieForm = document.getElementById("movieForm");
+});
